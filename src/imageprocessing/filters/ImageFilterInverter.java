257cfcffc5e59a965d930cfc6388aba3bb5a6c95
Gianlo <gianlorenzo.occhipinti@gmail.com>
2020-10-03 23:48:59 +0200

    Little fix

--- src/imageprocessing/filters/ImageFilterInverter.java
+++ src/imageprocessing/filters/ImageFilterInverter.java
@@ -15,14 +15,7 @@ public class ImageFilterInverter extends ImageFilterRaster<ImageFilterInverterSe
 
 	@Override
 	public void filterImagePixel(ImageFilterInverterSettings filterSettings, ImageAccessor filterImage, int imagePixelX, int imagePixelY, ImageColor imagePixelColor) {
-		
-		int channelR = imagePixelColor.getColorChannel(ImageColor.COLOR_CHANNEL_R);
-		int channelG = imagePixelColor.getColorChannel(ImageColor.COLOR_CHANNEL_G);
-		int channelB = imagePixelColor.getColorChannel(ImageColor.COLOR_CHANNEL_B);
-
-		imagePixelColor.setColorChannel(ImageColor.COLOR_CHANNEL_R, 255 - channelR);
-		imagePixelColor.setColorChannel(ImageColor.COLOR_CHANNEL_G, 255 - channelG);
-		imagePixelColor.setColorChannel(ImageColor.COLOR_CHANNEL_B, 255 - channelB);
+		imagePixelColor.colorInvert();
 	}
 
 	public static class ImageFilterInverterSettings extends ImageFilterSettings {
