af95f1474db5a4a6a9785f412808c8bcb5a46130
Emax <emax@emax.dev>
2020-10-03 23:23:42 +0200

    Refractoring

--- src/imageprocessing/engine/ImageEngine.java
+++ src/imageprocessing/engine/ImageEngine.java
@@ -10,9 +10,9 @@ import imageprocessing.filter.option.ImageFilterSettings;
 import imageprocessing.filters.ImageFilterAberration;
 import imageprocessing.filters.ImageFilterEdge;
 import imageprocessing.filters.ImageFilterExample;
-import imageprocessing.filters.ImageFilterSepia;
 import imageprocessing.filters.ImageFilterGreyscale;
 import imageprocessing.filters.ImageFilterInverter;
+import imageprocessing.filters.ImageFilterSepia;
 import imageprocessing.ui.options.ImageFilterOptionUIBuilder;
 import imageprocessing.utils.ImageSnapshots;
 import imageprocessing.utils.ImageUtils;
@@ -22,12 +22,12 @@ import javafx.scene.layout.VBox;
 public class ImageEngine {
 
 	public static final ImageFilter<?>[] IMAGE_FILTERS = {
-		new ImageFilterExample(),
 		new ImageFilterAberration(),
-		new ImageFilterEdge(),
-		new ImageFilterInverter()
+		new ImageFilterGreyscale(),
+		new ImageFilterInverter(),
+		new ImageFilterExample(),
 		new ImageFilterSepia(),
-		new ImageFilterGreyscale()
+		new ImageFilterEdge(),
 	};
 
 	private ImageFilter<?> imageFilter;
